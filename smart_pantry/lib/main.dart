d61206cd4150c1f0e77827da5837bdda83da5d1d
6731503034watthana <6731503034@lamduan.mfu.ac.th>
2026-02-21 15:00:55 +0700

    1.0

--- smart_pantry/lib/main.dart
+++ smart_pantry/lib/main.dart
@@ -1,12 +1,21 @@
 import 'package:flutter/material.dart';
+import 'package:firebase_core/firebase_core.dart'; // 1. เพิ่ม import ของ Firebase
 import 'package:provider/provider.dart';
 import 'package:smart_pantry/config/router.dart';
 import 'package:smart_pantry/core/constants/app_theme.dart';
 import 'package:smart_pantry/providers/food_items_provider.dart';
 import 'package:smart_pantry/providers/auth_provider.dart';
+import 'firebase_options.dart'; // 2. เพิ่ม import ไฟล์ตั้งค่า Firebase ที่ระบบสร้างให้
 
-void main() {
+// 3. เปลี่ยนเป็น async เพื่อให้รอ Firebase โหลดเสร็จก่อน
+void main() async { 
   WidgetsFlutterBinding.ensureInitialized();
+
+  // 4. สั่งเริ่มต้นการทำงานของ Firebase
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
+
   runApp(const SmartPantryApp());
 }
 
